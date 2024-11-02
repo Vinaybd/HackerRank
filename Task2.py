b27dad7751ce90bd06baebac2dd477cec248bca9
Vinaybd <vinaydacchu26@gmail.com>
2024-11-02 17:06:15 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,13 @@
+# Task 2
+# The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+
+if __name__ == '__main__':
+    a = int(input("Enter Value of a:"))
+    b = int(input("Enter Value of b:"))
+    print(a+b)
+    print(a-b)
+    print(a*b)
