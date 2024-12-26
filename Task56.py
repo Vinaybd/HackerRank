b571fcb35896d2a1d5fe46352839edc6917aca57
Vinaybd <vinaydacchu26@gmail.com>
2024-12-26 22:12:04 +0530

    Add files via upload

---
+++ Task56.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+#Divmod which takes 2 inputs and divide 2 inputs and return divison and remainder
+
+a = int(input("Enter an number:"))
+b = int(input("Enter an number:"))
+
+print(a//b)
+print(a % b)
+print(divmod(a, b))
