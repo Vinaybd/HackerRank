80aed50406e1e9d3ec95683d4fca2f62f4e9d24e
Vinaybd <vinaydacchu26@gmail.com>
2024-12-27 08:34:45 +0530

    Add files via upload

---
+++ Task57.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT\
+# So far, we have only heard of Python's powers. Now, we will witness them!
+
+# Powers or exponents in Python can be calculated using the built-in power function.
+# Call the power function a^b as shown below:
+
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+m=int(input("Enter the value of m:"))
+
+print((a**b)) #/?or print(pow(a,b))
+print(pow(a,b,m))
