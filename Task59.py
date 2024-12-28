a49798ec4fd05bace61df6aee6be00844199a828
Vinaybd <vinaydacchu26@gmail.com>
2024-12-28 22:17:25 +0530

    Add files via upload

---
+++ Task59.py
@@ -0,0 +1,9 @@
+# Read four numbers,a ,b ,c , and d, and print the result of a^b + c^ b.
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(a**b + c**d)
